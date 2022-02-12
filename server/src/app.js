2008be7fab805c78281467114dd67b95d3a1ca28
ldldz <qkrqudrjs4350@gmail.com>
2022-02-12 20:32:58 +0900

    Fix: set port

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('build'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/build/index.html');
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running!!');
 })
