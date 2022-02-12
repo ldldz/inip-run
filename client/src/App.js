2e4ab1456b3f0056bc49e5377114ba6da61366bd
ldldz <qkrqudrjs4350@gmail.com>
2022-02-12 20:00:07 +0900

    init

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      인입런🏃
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/build/index.html');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running!!');
+})
