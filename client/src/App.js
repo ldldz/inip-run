ad307e1373c05b7cf74940cae23ec6fb722f2975
ldldz <qkrqudrjs4350@gmail.com>
2022-02-27 22:42:04 +0900

    feat: 버스번호, 남은 시간 표시 div  추가

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState([]);
   const callApi = async () => {
-    axios.get("/api", { params: { bstopid: "164000395" } }).then((res) => {
+    axios.get("https://iniprun.herokuapp.com/api", { params: { bstopid: "164000395" } }).then((res) => {
       console.log(res.data);
       setData(res.data.map((v) => [v.ROUTEID._text, v.ARRIVALESTIMATETIME._text]));
     });
@@ -14,7 +14,13 @@ function App() {
     callApi();
   }, []);
 
-  return <div className="App">{data.join("\n")}</div>;
+  return (
+    <div className="App">
+      {data.map(([routeId, arrivalEstimateTime]) => (
+        <div>{`버스 번호: ${routeId} 남은 시간: ${parseInt(arrivalEstimateTime / 60)}분 ${arrivalEstimateTime % 60}초`}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
