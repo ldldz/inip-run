f29925be969934efdcb78317d930d7b99ed4f3fc
ldldz <qkrqudrjs4350@gmail.com>
2022-03-07 00:07:02 +0900

    feat: 도착예정시간 1초마다 감소

--- client/src/components/BusArrivalTimeListItem/index.js
+++ client/src/components/BusArrivalTimeListItem/index.js
@@ -1,10 +1,20 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
 const BusArrivalTimeListItem = ({busNo, arrivalEstimateTime}) => {
+  const [arrivalTime, setArrivalTime] = useState(arrivalEstimateTime);
+  useEffect(() => {
+    const timer = setInterval(() => {
+        setArrivalTime(arrivalTime => arrivalTime - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="busArrivalTimeListItem">
       <div className="busNo">{busNo}</div>
-      <div className="arrivalEstimateTime">{parseInt(arrivalEstimateTime / 60)}분 {arrivalEstimateTime % 60}초</div>
+      <div className="arrivalEstimateTime">{parseInt(arrivalTime / 60)}분 {arrivalTime % 60}초</div>
     </div>
   );
 };
